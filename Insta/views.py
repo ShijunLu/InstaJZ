2082696d02a1621b73174805fcefec6f8af1438b
ShijunLu <lshijun0000@ucla.edu>
2020-01-16 13:06:35 -0800

    Achieve Auth and header

--- Insta/views.py
+++ Insta/views.py
@@ -4,9 +4,13 @@ from django.views.generic import TemplateView, ListView, DetailView
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 
 from Insta.models import Post
+
+from Insta.forms import CustomUserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # HellowWorld is -a TemplateView
 
@@ -21,10 +25,11 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login' 
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -35,6 +40,13 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts')
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
+
+
 
 
 

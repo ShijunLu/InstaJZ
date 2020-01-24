98a4edb69717b6e4abc279b17d21da628390d15c
ShijunLu <lshijun0000@ucla.edu>
2020-01-23 22:21:30 -0800

    update whitenouisestorage

--- InstaJZ/settings.py
+++ InstaJZ/settings.py
@@ -125,8 +125,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'posts' 
-
+ 
 LOGOUT_REDIRECT_URL = 'posts'
 
 AUTH_USER_MODEL = 'Insta.InstaUser'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
